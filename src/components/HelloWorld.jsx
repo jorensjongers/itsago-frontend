3fecc7f001e823e0d273b3b82e4ff45228e42a2d
joren.sjongers <joren.sjongers@icloud.com>
2020-02-28 16:15:36 +0100

    Hello world

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+  render() {
+    return (
+    <div className="hello-world">
+      <h1>Hello World!</h1>
+    </div>
+    );
+  }
+}
+export default HelloWorld;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+
+// This library allows us to combine paths easily
+const path = require('path');
+module.exports = {
+   entry: path.resolve(__dirname, 'src', 'index.jsx'),
+   output: {
+      path: path.resolve(__dirname, 'output'),
+      filename: 'bundle.js'
+   },
+   resolve: {
+      extensions: ['.js', '.jsx']
+   },
+   devServer: {
+      contentBase: './src',
+      publicPath: '/output'
+   },
+   module: {
+      rules: [
+         {
+             test: /\.jsx/,
+             use: {
+                loader: 'babel-loader',
+                options: { presets: ['@babel/preset-react', '@babel/preset-env'] }
+             }
+         },
+         {
+            test: /\.scss/,
+            use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'] 
+            // Note that postcss loader must come before sass-loader
+         }
+      ]
+   }
+};
