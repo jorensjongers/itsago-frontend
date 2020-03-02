be0108ccc91aadd175067fe1ab7fb783e367a77a
joren.sjongers <joren.sjongers@icloud.com>
2020-03-02 18:30:47 +0100

    refactor uploadImage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Loader from 'react-loader-spinner'
 import CameraField from './CameraField.jsx';
 
-const API_URL = "http://localhost:5000"
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -12,24 +11,12 @@ class App extends React.Component {
       }; 
   }
 
-  uploadImage = (picture) => {
-    this.setState({uploading: true});
-    console.log(picture);
-    const formData = new FormData();
-    formData.append('file', picture, 'image.jpg');
-    fetch(API_URL + "/upload_image", {
-      method: 'POST',
-      body: formData
-    });
-    this.setState({uploading: false})
-  }
-
   render() {
     const content = () => {
         if (this.state.uploading) {
           return <Loader type='Grid' />;
         } else {
-          return <CameraField txt='Upload image' action={this.uploadImage} />;
+          return <CameraField txt='Upload image'/>;
         };
       }
 
--- src/components/CameraField.jsx
+++ src/components/CameraField.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Camera from 'react-camera'
 import Button from './Button'
+const API_URL = "http://localhost:5000";
 
 class CameraField extends React.Component {
     render() {
@@ -11,10 +12,21 @@ class CameraField extends React.Component {
     
         )
     }
+    uploadImage = (picture) => {
+        this.setState({uploading: true});
+        console.log(picture);
+        const formData = new FormData();
+        formData.append('file', picture, 'image.jpg');
+        fetch(API_URL + "/upload_image", {
+          method: 'POST',
+          body: formData
+        });
+        this.setState({uploading: false})
+    }
 
     takePicture = () => {
         this.camera.capture()
-        .then(picture => {this.props.action(picture)});
+        .then(picture => {this.uploadImage(picture)});
     }
 }
 
