ebb11820436bd32ed0a69cbc53e7908599e405a8
joren.sjongers <joren.sjongers@icloud.com>
2020-03-16 21:08:12 +0100

    add search results

--- src/components/screens/InputScreen.jsx
+++ src/components/screens/InputScreen.jsx
@@ -55,17 +55,27 @@ class CameraScreen extends React.Component {
 class ManualEntry extends React.Component {
   state = {
     query: '',
+    results: []
   }
  
   handleInputChange = () => {
-    this.setState({
-      query: this.search.value
-    })
+    this.setState({query: this.search.value}, this.getInfo);
+  }
+
+  getInfo = () => {
+    if (this.state.query && this.state.query.length > 0) {
+      const url = 'http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=' + this.state.query + '&api_key=b264cc6a326b887db91199d794593d58&format=json';
+      fetch(url)
+      .then((response) => response.json())
+      .then((data) => this.setState({results: data.results.artistmatches.artist.slice(0,7)}));
+    } else {
+      this.setState({results: []})
+    }
   }
 
   render() {
     return (
-      <form>
+      <form className='autocomplete'>
         <input 
           type="text" 
           className="input" 
@@ -73,9 +83,20 @@ class ManualEntry extends React.Component {
           ref={input => this.search = input} 
           onChange={this.handleInputChange}
         />
-        <p>{this.state.query}</p>
+        <Suggestions results={this.state.results} />
       </form>
     );
+  }
+}
+
+class Suggestions extends React.Component {
+  render() {
+    const options = this.props.results.map( r => (
+      <li key={r.name} className='autocomplete-items'>
+        {r.name}
+      </li>
+    ))
+    return <ul className='autocomplete-list'>{options}</ul>
   }
 }
 
