006d4416e9796ac11d87ceb127ec6142cf4022c4
joren.sjongers <joren.sjongers@icloud.com>
2020-03-23 15:23:47 +0100

    updaate image paths

--- src/components/screens/StartScreen.jsx
+++ src/components/screens/StartScreen.jsx
@@ -5,7 +5,7 @@ class StartScreen extends React.Component {
     render() {
         return (
             <div className='screen'>
-                <img className='bac-logo' src='./images/bac_logo.png'/>
+                <img className='bac-logo' src='.src//images/bac_logo.png'/>
                 <h1 className='placeholder'> Short explanation goes here </h1>
                 <button onClick={() => this.props.changeState('input')}> Start! </button>
             </div>
--- src/components/screens/WelcomeScreen.jsx
+++ src/components/screens/WelcomeScreen.jsx
@@ -4,9 +4,9 @@ export class WelcomeScreen extends Component {
     render() {
         return (
             <div className='screen'>
-                <img className='bac-logo' src='./images/bac_logo.png'/>
+                <img className='bac-logo' src='./src/images/bac_logo.png'/>
                 <h1>Welcome to Brussels Airport Luggage Check</h1>
-                <img className='luggage' src='./images/luggage.png'/>
+                <img className='luggage' src='./src/images/luggage.png'/>
                 <h6> By clicking continue you agree to our </h6>
                 <h6 className='link' onClick={() => this.props.changeState('terms')}> Terms and Conditions </h6>
                 <button onClick={() => this.props.changeState('start')}> Continue </button>
