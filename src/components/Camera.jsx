73e37c04270feb93ae577a737d98aa7ab9898107
joren.sjongers <joren.sjongers@icloud.com>
2020-03-29 14:35:04 +0200

    other camera component

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -1,48 +1,20 @@
 import React from 'react'
-import Camera from 'react-camera'
+import Camera from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
 const API_URL = "http://localhost:5000";
 
 
 class CameraScreen extends React.Component {
 
-
-    render() {
-        return (
-          <CameraField 
-            setPath={(path) => this.props.setPath(path)}
-            setItem={(item) => this.props.setItem(item)}
-            continue={this.props.continue}
-          />
-        )
-    }
-}
-
-class CameraField extends React.Component {
-
-    render() {
-        return (
-          <div>
-            <div className='container'>
-              <Camera className="preview" ref={(cam) => {this.camera = cam;}}/>
-            </div>
-            <button onClick={this.takePicture}> Take picture </button>
-          </div>
-        );
-    }
-
-    takePicture = () => {
-        this.camera.capture()
-        .then(blob => 
-          { 
-            this.props.setPath(URL.createObjectURL(blob));
-            this.uploadImage(blob);
-          }
-        );
+    takePicture = (dataUri)  => {
+      const picture = fetch(dataUri)
+      this.props.setPath(dataUri)
+      this.uploadImage(picture)
+      
     }
 
     uploadImage = (picture) => {
       /*
-      console.log(picture);
       const formData = new FormData();
       formData.append('file', picture, 'image.jpg');
       fetch(API_URL + "/upload_image", {
@@ -53,9 +25,16 @@ class CameraField extends React.Component {
       /* TODO: set this to backend result */
       this.props.setItem('Your face');
       this.props.continue();
+    }
 
-  }
-};
+    render() {
+        return (
+          <Camera
+            onTakePhoto = {(dataUri) => {this.takePicture(dataUri);}}
+          />
+        )
+    }
+}
 
 
 export default CameraScreen
