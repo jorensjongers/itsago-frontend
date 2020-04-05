2d83e86258d6180364bd039eb8662f289a63fb6d
joren.sjongers <joren.sjongers@icloud.com>
2020-04-05 15:45:36 +0200

    welcome screen checkbox

--- src/components/screens/WelcomeScreen.jsx
+++ src/components/screens/WelcomeScreen.jsx
@@ -1,17 +1,44 @@
 import React, { Component } from 'react'
 
 export class WelcomeScreen extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            agreed: false
+        }
+    }
+
+    continueButton = () => {
+        if (this.state.agreed) {
+            return <button onClick={() => this.props.changeState('input')}> Continue </button>
+        } else {
+            return <button className='disabled'> Continue </button>
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({agreed: event.target.checked})
+    } 
     render() {
         return (
             <div className='screen'>
                 <img className='bac-logo' src='./src/images/bac_logo.png'/>
                 <img className='language-icon' src='./src/images/language_icon.png'/>
-                <h1 className='product-name'> Brussels Airport Hand Baggage Check </h1>
+                <h1> Brussels Airport Hand Baggage Check </h1>
                 <h2> Developed by </h2>
                 <img className='itsago' src='./src/images/itsago.png'/>
-                <h6> By clicking continue you agree to our </h6>
-                <button onClick={() => this.props.changeState('input')}> Continue </button>
+                <label htmlFor="terms">
+                    <input 
+                        type="checkbox"
+                        id='terms'
+                        checked={this.state.agreed}
+                        ref='agreed'
+                        onChange={this.handleChange}/>
+                    I agree to the 
+                 </label>   
                 <h6 className='link' onClick={() => this.props.changeState('terms')}> Terms and Conditions </h6>
+                {this.continueButton()}
             </div>
         )
     }
